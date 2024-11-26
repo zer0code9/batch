825ac091979ba9735258e88c2a99aa39a8e2e74c
zer0code9 <asorinus@gmail.com>
2024-11-26 13:30:56 -0800

    New

--- src/jvalt/Group.java
+++
@@ -1,9 +0,0 @@
-package jvalt;
-
-public interface Group {
-	public int size();
-	public Object get(int index);
-	public int indexOf(Object o);
-	public int lastIndexOf(Object o);
-	public int[] indicesOf(Object o);
-}
